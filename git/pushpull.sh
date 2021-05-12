a6d9d2a3906e8733dbf759e58b6e8cd2f234e455
Neil Johnson <neil@cadent.com>
2021-05-12 13:58:20 -0400

    Add status check

---
+++ git/pushpull.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "Attemping to push and pull changes in directory:"
+pwd
+echo "Pulling the latest changes using rebase ..."
+git pull --rebase
+E_PULL=$?
+# if it's not zero, it's an error
+if [ $E_PULL -ne 0 ]; then
+    echo "Error $E_PULL - update cancelled."
+    exit $E_PULL
+fi
+echo "Checking for local changes to push ..."
+changed_files=$(git status -s)
+E_STATUS=$?
+# if it's not zero, it's an error
+if [ $E_STATUS -ne 0 ]; then
+    echo "Error $E_STATUS - update cancelled."
+    exit $E_STATUS
+else
+    echo "$changed_files"
+    echo "Add all changed files?" 
+    read -p "Enter Y to add, N to exit: " addFiles
+fi
+
