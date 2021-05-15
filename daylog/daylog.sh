6b4448724e0d358cc0ccf2f20078dc91be74720b
Neil Johnson <neil@cadent.com>
2021-05-15 10:47:32 -0400

    Script is working, but can't fix line 31

--- daylog/daylog.sh
+++ daylog/daylog.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 # daylog.sh creates a file in the specified directory 
 # and opens it with your favorite editor
-
+# TODO: Set default values for config vars
+# TODO: Check for existence of config file
 source daylog.cfg
 DAYLOG_NAME="log-$(date +%Y-%m-%d).md"
 
-echo $0
-echo "Creates a new daylog file $DAYLOG_NAME in directory"
-echo "$DAYLOG_DIR and edit with $EDITOR_APP"
+echo $(basename $0)
+echo "Create a new daylog file $DAYLOG_NAME in directory"
+echo "$DAYLOG_DIR then edit with $EDITOR_APP"
 
 cd "$REPO_DIR"
-
+echo "Working in repository:"
+pwd
 echo "Pulling the latest changes using rebase ..."
 git pull --rebase
 E_PULL=$?
@@ -23,5 +25,13 @@ if [ $E_PULL -ne 0 ]; then
 fi
 # Append an H2 timestamp to the file
 TIME_STAMP="## $(date +%H:%M)"
-echo $TIME_STAMP >> "$DAYLOG_DIR$DAYLOG_NAME"
-$("$EDITOR_APP) $DAYLOG_DIR$DAYLOG_NAME"
+PATH_FILE="$DAYLOG_DIR$DAYLOG_NAME"
+echo "Appending time stamp to log file $PATH_FILE"
+echo $TIME_STAMP >> $PATH_FILE
+$("$EDITOR_APP" "$PATH_FILE")
+# stage and push to git
+# TODO: git status check for changes
+git add $PATH_FILE
+git_msg="Update by daylog.sh"
+git commit -m "$git_msg"
+git push
