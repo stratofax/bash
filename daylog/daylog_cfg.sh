b4a6d38d1dcd29b0f708843655f26c4f0b920816
Neil Johnson <neil@cadent.com>
2023-03-06 22:39:24 -0500

    Format output

--- daylog/daylog_cfg.sh
+++ daylog/daylog_cfg.sh
@@ -25,32 +25,46 @@ CONFIG_HERE=$CONFIG_PATH/$CONFIG_FILE
 # Functions
 #######################################
 
-# check to see if the repository directory exists
+function print_line( ) {
+    color_echo "${B_WHITE}" "-------------------------------------------------------"
+}
+
+# check to see if the directory exists
 function check_dir( ) { 
     dir_name=$1
     description=$2
+    dir_help=$3
     if [ ! -d $dir_name ]; then
+        color_echo "${B_WHITE}" "The ${description} directory is"
+        color_echo "${B_WHITE}" "${dir_help}"
+        print_line
         color_echo "${B_RED}" "The $description directory was not found:"
         color_echo "${B_YELLOW}" "${dir_name}"
+        print_line
         color_echo "${B_CYAN}" "Please enter a new $description directory:"
         read -r dir_name
-        check_dir
+        check_dir $dir_name $description $dir_help
     else
         color_echo "${WHITE}" "Current $description directory:"
         color_echo "${B_YELLOW}" "${dir_name}"
+        print_line
     fi
 }
 
 #######################################
 # display a welcome message
 # use the color_echo function from colors.sh
+
+print_line
 color_echo "${B_WHITE}" "Welcome to the daylog config tool"
+print_line
 
 # Check for external config file
 if [ ! -f $CONFIG_HERE ]; then
     color_echo "${YELLOW}" "Configuration file not found:"
     color_echo "${B_YELLOW}" "${CONFIG_FILE}"
     color_echo "${CYAN}" "Using default configuration settings."
+    print_line
     # DO NOT enclose the tilde character ("~") in quotes (double or single)
     # as this prevents path expansion. To specify a directory name with
     # spaces, add the quotes AFTER the tidle, like this:
@@ -69,5 +83,5 @@ fi
 color_echo "${B_WHITE}" "Current configuration"
 
 # review the repository directory
-check_dir $repo_dir "repository"
-
+check_dir $repo_dir "repository" "the root of the repository that contains your daylogs"
+check_dir $daylog_dir "daylog" "The specific directory that contains your daylogs"
